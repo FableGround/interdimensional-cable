8f026891d458b47c3f6637e4c4866174aa83df39
Daniel Ravina <raabhim@gmail.com>
2026-06-08 13:05:45 -0700

    tweak config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  ...(process.env.GITHUB_PAGES === "true" ? { basePath: "/interdimensional_cable" } : {}),
 };
 
 export default nextConfig;
